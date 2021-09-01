cf88dee950821b1794f88dab896e9032cb09b6b8
aditinayak312 <83701303+aditinayak312@users.noreply.github.com>
2021-09-01 23:44:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 var pictures=["Screenshot (327).png","Dassam_falls2.jpg","hundru falls.jpg","Jonha_falls.jpg","palani falls.jpg","Hirni falls.jpg","bhatinda falls.png","baghmunda falls.jpg","sita falls.jpg"];
 var waterfalls=["SOME WATERFALLS OF JHARKHAND","DASSAM FALLS","HUNDRU FALLS","JONHA FALLS","PALANI FALLS","HIRNI WATERFALLS","BHATINDA FALLS","BAGHMUNDA FALLS","SITA FALLS"];
 var count=0;
-function change(){
+function change1(){
     if(count > 8){
         count=0;
     }
-    document.getElementById("image").src=pictures[count];
+    document.getElementById("wfimage").src=pictures[count];
     document.getElementById("names").innerHTML=waterfalls[count];
+    count++;
+
+}
+var cm=["satyameva jayate2.jpg","Babulal.jpg","arjun munda.jpg","Shibu_Soren.jpg","arjun munda.jpg","madu koda.jpg","Shibu_Soren.jpg","arjun munda.jpg","hemnat soren.jpg","raghubar das.jpg","hemnat soren.jpg"];
+var cfmname=["Chief ministers of Jharkhand","Babulal Marandi","Arjun Munda","Shibu Soren","Arjun Munda","Madhu Koda","Shibu Soren","Arjun Munda","Hemant Soren","Raghubar Das","Hemant Soren"];
+var count=0;
+function change(){
+    if(count > 10){
+        count=0;
+    }
+    document.getElementById("cm").src=cm[count];
+    document.getElementById("cmname").innerHTML=cfmname[count];
     count++;
 }
